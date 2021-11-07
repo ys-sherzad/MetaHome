d2a9c45e3b11587e884d36cf0401efc9fcc022cc
YasirSherzad <ys.sherzad@gmail.com>
2021-11-07 15:26:44 +0430

    update Card design, add control_room_model

---
+++ lib/repo/control_room_model.dart
@@ -0,0 +1,62 @@
+// A read-only description of a Control Room
+class ControlRoom {
+  ControlRoom({
+    required this.id,
+    required this.name,
+    required this.countOfDevices,
+    required this.isOn,
+  });
+
+  final String id;
+  final String name;
+  final int countOfDevices;
+  final bool isOn;
+
+  @override
+  String toString() {
+    return 'ControlRoom{id: $id, name: $name, countOfDevices: $countOfDevices, isOn: $isOn}';
+  }
+}
+
+class ControlRoomList {
+  ControlRoomList();
+
+  static List<ControlRoom> controlRooms = [
+    ControlRoom(
+      id: '1',
+      name: 'Living Room',
+      countOfDevices: 5,
+      isOn: false,
+    ),
+    ControlRoom(
+      id: '2',
+      name: 'Bathroom',
+      countOfDevices: 1,
+      isOn: false,
+    ),
+    ControlRoom(
+      id: '3',
+      name: 'Bedroom',
+      countOfDevices: 1,
+      isOn: false,
+    ),
+    ControlRoom(
+      id: '4',
+      name: 'Dining Room',
+      countOfDevices: 1,
+      isOn: false,
+    ),
+    ControlRoom(
+      id: '5',
+      name: 'Kitchen',
+      countOfDevices: 4,
+      isOn: false,
+    ),
+    ControlRoom(
+      id: '6',
+      name: 'Garage',
+      countOfDevices: 2,
+      isOn: false,
+    ),
+  ];
+}
--- lib/widgets/home/control_room.dart
+++ lib/widgets/home/control_room.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:meta_home/repo/control_room_model.dart';
+import 'package:meta_home/utils.dart';
+import 'package:meta_home/widgets/home/toggle_button.dart';
+import 'package:meta_home/repo/control_room_model.dart';
 
 BoxDecoration outline(double radius) {
   return BoxDecoration(
@@ -8,44 +12,43 @@ BoxDecoration outline(double radius) {
   );
 }
 
-Widget EmptyContainer() {
-  return Container(
-    // height: 170.0,
-    // width: 50.0,
-    // margin: EdgeInsets.all(10.0),
-    decoration: outline(20.0),
-    padding: EdgeInsets.symmetric(
-      horizontal: 20.0,
-      vertical: 24.0,
-    ),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text('Icon'),
-        Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [Text('Living Room'), Text('5 devices')],
-          ),
-        ),
-        Container(
-          height: 30.0,
-          width: 56.0,
-          decoration: outline(20.0),
-          child: null,
-        )
-      ],
-    ),
-  );
-  // margin: EdgeInsets.all(5.0));
-}
-
 class ControlRoom extends HookWidget {
   const ControlRoom({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Widget Card({
+      required String name,
+      required int countOfDevices,
+      required bool isOn,
+    }) {
+      return Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: ColorLib.border),
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: 20.0,
+          vertical: 24.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Icon'),
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [Text(name), Text('$countOfDevices device')],
+              ),
+            ),
+            ToggleButton(),
+          ],
+        ),
+      );
+      // margin: EdgeInsets.all(5.0));
+    }
+
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 20.0),
       child: GridView.count(
@@ -53,9 +56,14 @@ class ControlRoom extends HookWidget {
         mainAxisSpacing: 28.0,
         crossAxisCount: 2,
         // childAspectRatio: 3 / 2,
-        children: List.generate(10, (index) {
-          return EmptyContainer();
-        }),
+        children: [
+          for (var room in ControlRoomList.controlRooms)
+            Card(
+              name: room.name,
+              countOfDevices: room.countOfDevices,
+              isOn: room.isOn,
+            ),
+        ],
       ),
     );
   }
