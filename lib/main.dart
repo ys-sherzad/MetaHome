1492eb504ef55280e4073115b17f8577044fe8fb
YasirSherzad <ys.sherzad@gmail.com>
2021-11-08 02:12:18 +0430

    set fontFamily to Poppins

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,9 @@ class MyApp extends StatelessWidget {
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
+
         // is not restarted.
+        fontFamily: 'Poppins',
         primarySwatch: Colors.blue,
       ),
       home: Home(),
