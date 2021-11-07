46c0d86b338eba82ad4ad110c375ae8bee7af9a3
YasirSherzad <ys.sherzad@gmail.com>
2021-11-08 02:14:55 +0430

    adjust styles and remove unused code

--- lib/widgets/home/power_usage_today.dart
+++ lib/widgets/home/power_usage_today.dart
@@ -2,7 +2,6 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
-// import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../utils.dart';
 
 BoxDecoration _decoration() {
@@ -17,29 +16,29 @@ const SizedBox Spacer = SizedBox(width: 15.0);
 class PowerUsageToday extends HookWidget {
   PowerUsageToday({Key? key}) : super(key: key);
 
-  var KiloWattText = RichText(
-    text: TextSpan(
-      text: '30.4',
-      style: TextStyle(
-        color: Colors.black,
-        fontSize: 24.0,
-        fontWeight: FontWeight.w800,
-      ),
-      children: <TextSpan>[
-        TextSpan(
-            text: ' kWh',
-            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 16),
-            recognizer: TapGestureRecognizer()..onTap = () {}),
-      ],
-    ),
-  );
-
   @override
   Widget build(BuildContext context) {
+    var KiloWattText = RichText(
+      text: TextSpan(
+        text: '30.4',
+        style: TextStyle(
+          color: ColorLib.primary,
+          fontSize: 23.0,
+          fontWeight: FontWeight.w800,
+        ),
+        children: <TextSpan>[
+          TextSpan(
+              text: ' kWh',
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15),
+              recognizer: TapGestureRecognizer()..onTap = () {}),
+        ],
+      ),
+    );
+
     return Container(
       height: 100.0,
-      margin: EdgeInsets.symmetric(horizontal: LAYOUT_HORIZONTAL_MARGIN),
-      padding: EdgeInsets.symmetric(horizontal: 26.0),
+      margin: const EdgeInsets.symmetric(horizontal: LAYOUT_HORIZONTAL_OFFSET),
+      padding: const EdgeInsets.symmetric(horizontal: 26.0),
       decoration: _decoration(),
       child: Row(
         // crossAxisAlignment: CrossAxisAlignment.center,
@@ -51,7 +50,7 @@ class PowerUsageToday extends HookWidget {
               borderRadius: BorderRadius.circular(50),
               color: Color.fromRGBO(237, 162, 88, 1),
             ),
-            child: Icon(
+            child: const Icon(
               FontAwesome.flash,
               size: 35.0,
               color: Colors.white,
@@ -64,7 +63,7 @@ class PowerUsageToday extends HookWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 KiloWattText,
-                SizedBox(height: 2.0),
+                const SizedBox(height: 2.0),
                 Text(
                   'Power usage today',
                   style: TextStyle(
