6a463367337ec2807ed92cd7ba97f21048184a17
YasirSherzad <ys.sherzad@gmail.com>
2021-11-08 02:14:26 +0430

    adjust styles and remove unused code

--- lib/widgets/home/header.dart
+++ lib/widgets/home/header.dart
@@ -2,23 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import '../../utils.dart';
 
-// double HEADER_HEIGHT = 70.0;
-// double LAYOUT_HORIZONTAL_MARGIN = 20.0;
-
-BoxDecoration outline(double radius) {
-  return BoxDecoration(
-    border: Border.all(color: Colors.black),
-    borderRadius: BorderRadius.circular(radius),
-  );
-}
-
 class Header extends HookWidget {
   const Header({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: LAYOUT_HORIZONTAL_MARGIN),
+      margin: const EdgeInsets.symmetric(horizontal: LAYOUT_HORIZONTAL_OFFSET),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -26,9 +16,8 @@ class Header extends HookWidget {
             flex: 1,
             child: Container(
               margin: EdgeInsets.only(right: 20.0),
-              padding: EdgeInsets.symmetric(vertical: 10.0),
+              padding: const EdgeInsets.symmetric(vertical: 5.0),
               height: HEADER_HEIGHT,
-              // decoration: outline(20.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -36,17 +25,17 @@ class Header extends HookWidget {
                   Text(
                     'Welcome home,',
                     style: TextStyle(
-                      fontSize: 14.0,
+                      fontSize: 16.0,
                       color: ColorLib.textSecondary,
                       fontWeight: FontWeight.w500,
                     ),
                   ),
-                  SizedBox(height: 4.0),
+                  // SizedBox(height: 4.0),
                   Text(
-                    'Yasir Sherzad',
+                    'Andrey Thomson',
                     style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w800,
+                      fontSize: 21.0,
+                      fontWeight: FontWeight.w700,
                     ),
                   ),
                 ],
